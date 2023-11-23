295312b3dd598fef4b800bfd265ea489b8f9eaa3
Felipe Getial <bfgetial@gmail.com>
2023-11-22 22:33:33 -0500

    starting work responsive design in projects

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -4,18 +4,18 @@ import "../styles/projectComponent.css";
 export default function ProjectComponent({ title, description, imageUrl }) {
   return (
     <div className="project">
-      <h4>{title}</h4>
-      <div>
+      <h4 className="projectTitle">{title}</h4>
+      <div className="projectDescription">
         <div>
           <p>{description}</p>
-          <div className="btnsDesktop">
+          <div className="btns btnsDesktop">
             <button>Ver Proyecto</button>
             <button>Ver Codigo</button>
           </div>
         </div>
         <img src={imageUrl} />
       </div>
-      <div className="btnsMobile">
+      <div className="btns btnsMobile">
         <button>Ver Proyecto</button>
         <button>Ver Codigo</button>
       </div>
