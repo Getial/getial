34cad10a846979d930ebc1a4284d76243fbb48be
Felipe Getial <bfgetial@gmail.com>
2024-02-09 22:36:44 -0500

    texts in projectDetail screen

--- src/screens/DetailProjectScreen.jsx
+++ src/screens/DetailProjectScreen.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Form, useNavigate, useLoaderData } from "react-router-dom";
 import { animateScroll } from "react-scroll";
 import { ArrowBack } from "@mui/icons-material";
-import { projects } from "../utils/projects";
+// import { projects } from "../utils/projects";
+import { useLang } from "../context/LangContext";
+import { getTexts } from "../utils/textos";
 import "../styles/projectDetail.css";
 
 export async function loader({ params }) {
-  const project = projects.find(
-    (project) => project.id === parseInt(params.projectId)
-  );
-  return { project };
+  const projectId = parseInt(params.projectId);
+  return { projectId };
 }
 
 export default function DetailProjectScreen() {
-  const { project } = useLoaderData();
+  const { projectId } = useLoaderData();
+  const [project, setProject] = useState();
+  const [textsComponent, setTextsComponent] = useState();
+  const { lang } = useLang();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const texts = getTexts(lang);
+    const res = texts.projects.find((project) => project.id === projectId);
+    setTextsComponent(texts.detailProject);
+    setProject(res);
+  }, [lang, projectId]);
+
+  //useEffect para hacer scroll top
   useEffect(() => {
     const options = {
       duration: 0,
@@ -27,34 +38,34 @@ export default function DetailProjectScreen() {
   return (
     <div className="projectDetail">
       <ArrowBack className="btn-back" onClick={() => navigate(-1)} />
-      <h1>{project.title}</h1>
+      <h1>{project?.title}</h1>
       <div className="left">
-        <img src={`.${project.image}`} alt="image project" />
+        <img src={`.${project?.image}`} alt="image project" />
         <div className="btns">
           <button>
-            <a href={project.codeUrl} target="_blank">
-              Ver Codigo {project.type === "Fullstack" && "Frontend"}
+            <a href={project?.codeUrl} target="_blank">
+              {textsComponent?.btnShowFrontend}
             </a>
           </button>
-          {project.demoUrl ? (
+          {project?.demoUrl ? (
             <button>
-              <a href={project.demoUrl} target="_blank">
-                Probar demo
+              <a href={project?.demoUrl} target="_blank">
+                {textsComponent?.btnTryDemo}
               </a>
             </button>
           ) : (
             <button>
-              <a href={project.backUrl} target="_blank">
-                Ver codigo Backend
+              <a href={project?.backUrl} target="_blank">
+                {textsComponent?.btnShowbackend}
               </a>
             </button>
           )}
         </div>
-        <p className="copy">{project.copy}</p>
+        <p className="copy">{project?.copy}</p>
       </div>
       <div className="right">
-        <h2>Caracteristicas</h2>
-        {project.characteristics.map((item, index) => (
+        <h2>{textsComponent?.characteristics}</h2>
+        {project?.characteristics.map((item, index) => (
           <div key={index} className="characteristic">
             <h3>{item.title}</h3>
             <p>{item.description}</p>
